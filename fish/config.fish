237e88bd99ddfc28005f85c9eab79d9ffe9b0cf6
Wesley Ryder <wr140815@ohio.edu>
2022-03-24 20:42:10 -0400

    init for nvim, still wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,21 @@
-set -g GREP_OPTIONS '--color=always'
-
+#set -g GREP_OPTIONS '--color=always'
 # Set dir colors
-eval ( dircolors --c-shell $HOME/.dircolors.256dark )
+#eval ( dircolors --c-shell $HOME/.dircolors.256dark )
 
-kitty + complete setup fish | source
+#kitty + complete setup fish | source
+
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+set -gx EDITOR nvim
+
+starship init fish | source
